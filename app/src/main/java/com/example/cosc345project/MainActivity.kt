96edcef3c31024d4cecc32c18662b6c4264d470e
Damo <damienmiller51@gmail.com>
2022-08-23 17:31:31 +1200

    Activity bar and Analytics page

--- app/src/main/java/com/example/cosc345project/MainActivity.kt
+++ app/src/main/java/com/example/cosc345project/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.cosc345project
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.cosc345project.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,15 +35,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //option actions
+        val id = item.itemId
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        if(id == R.id.action_item_home)
+            navController.navigate(R.id.HomeFragment)
+        if(id == R.id.action_item_analytics)
+            navController.navigate(R.id.AnalyticsFragment)
+
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_item_Settings -> true
+            R.id.action_item_analytics -> true
+            R.id.action_item_home -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
