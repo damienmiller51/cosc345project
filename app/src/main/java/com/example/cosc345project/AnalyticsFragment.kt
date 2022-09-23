5604a09217f6ea8088f2206382bf77801eb9088d
Denzel Lozano <dlozan21@gmail.com>
2022-09-23 22:52:29 +1200

    Analytics Fragment - added Mood Graph, added Daily Averages Text, changed format of date to dd format to reduce label cluttering

--- app/src/main/java/com/example/cosc345project/AnalyticsFragment.kt
+++ app/src/main/java/com/example/cosc345project/AnalyticsFragment.kt
@@ -51,9 +51,6 @@ class AnalyticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // create graphView for Sleep
-        val sleepGraph = binding.analyticsSleepGraph
-
         //formatting and initialising for weekArray
         val sdf = SimpleDateFormat("dd/MM/yyyy")
         val calendar = Calendar.getInstance()
@@ -65,33 +62,43 @@ class AnalyticsFragment : Fragment() {
             calendar.add(Calendar.DATE, -1)
         }
 
+        // GRAPHVIEW SLEEP
+
+        val sleepGraph = binding.analyticsSleepGraph
         val seriesSleep = LineGraphSeries<DataPoint>()
+        var sleepDailyAverage = 0
 
         // obtains all sleep data for corresponding days, reformats then adds to the data series
         for (i in 0..6) {
             val date = sdf.format(weekArray[i]).replace("/", "")
             val dateData = binding.root.context.getSharedPreferences(date,MODE_PRIVATE)
             val sleepData = dateData.getInt("sleep", 0)
+            sleepDailyAverage += sleepData
             seriesSleep.appendData(DataPoint(weekArray[i], sleepData.toDouble()),
                 true, 7)
         }
 
+        sleepDailyAverage = sleepDailyAverage/7
         sleepGraph.addSeries(seriesSleep)
 
-        // date label formatter to set it as MM/dd/yy
-        sleepGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(activity)
-        sleepGraph.gridLabelRenderer.numHorizontalLabels = 4
 
-        // setting x axis divisions
-        sleepGraph.viewport.setMinX(weekArray[0].time.toDouble())
-        sleepGraph.viewport.setMaxX(weekArray[6].time.toDouble())
-        sleepGraph.viewport.isXAxisBoundsManual
+        // date label formatter to set it as MM/dd/yy
+        sleepGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(activity,
+            SimpleDateFormat("dd"))
+        sleepGraph.gridLabelRenderer.numHorizontalLabels = 7
+        sleepGraph.gridLabelRenderer.horizontalAxisTitle = "Date"
         sleepGraph.gridLabelRenderer.setHumanRounding(false)
 
-        // create graphView for Steps
-        val stepsGraph = binding.analyticsStepsGraph
+        // setting x axis divisions
+        sleepGraph.viewport.isXAxisBoundsManual
+        sleepGraph.viewport.setMinX(weekArray[0].time.toDouble())
+        sleepGraph.viewport.setMaxX(weekArray[6].time.toDouble())
 
+        // GRAPHVIEW STEPS
+
+        val stepsGraph = binding.analyticsStepsGraph
         val seriesSteps = LineGraphSeries<DataPoint>()
+        var stepsDailyAverage = 0
 
         // obtains all steps data for corresponding days, reformats to Key:date's required structure
         // then adds to the data series
@@ -99,22 +106,76 @@ class AnalyticsFragment : Fragment() {
             val date = sdf.format(weekArray[i]).replace("/", "")
             val dateData = binding.root.context.getSharedPreferences(date,MODE_PRIVATE)
             val stepsData = dateData.getInt("steps", 0)
+            stepsDailyAverage += stepsData
             seriesSteps.appendData(DataPoint(weekArray[i], stepsData.toDouble()),
                 true, 7)
         }
 
+        stepsDailyAverage = stepsDailyAverage/7
         stepsGraph.addSeries(seriesSteps)
 
         // date label formatter to set it as MM/dd/yy
-        stepsGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(activity)
-        stepsGraph.gridLabelRenderer.numHorizontalLabels = 4
+        stepsGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(activity,
+            SimpleDateFormat("dd"))
+        stepsGraph.gridLabelRenderer.numHorizontalLabels = 7
+        stepsGraph.gridLabelRenderer.horizontalAxisTitle = "Date"
+        stepsGraph.gridLabelRenderer.setHumanRounding(false)
+        stepsGraph.gridLabelRenderer.labelVerticalWidth
 
         // setting x axis divisions
+        stepsGraph.viewport.isXAxisBoundsManual
         stepsGraph.viewport.setMinX(weekArray[0].time.toDouble())
         stepsGraph.viewport.setMaxX(weekArray[6].time.toDouble())
-        stepsGraph.viewport.isXAxisBoundsManual
-        stepsGraph.gridLabelRenderer.setHumanRounding(false)
 
+        // GRAPHVIEW MOOD
+
+        val moodGraph = binding.analyticsMoodGraph
+        val seriesMood = LineGraphSeries<DataPoint>()
+        var moodDailyAverage = 0
+
+        // obtains all mood data for corresponding days, reformats to Key:date's required structure
+        // then adds to the data series
+        for (i in 0..6) {
+            val date = sdf.format(weekArray[i]).replace("/", "")
+            val dateData = binding.root.context.getSharedPreferences(date,MODE_PRIVATE)
+            val moodData = dateData.getInt("water", 0)
+            moodDailyAverage += moodData
+            seriesMood.appendData(DataPoint(weekArray[i], moodData.toDouble()),
+                true, 7)
+        }
+
+        moodDailyAverage = moodDailyAverage/7
+        moodGraph.addSeries(seriesMood)
+
+        // date label formatter to set it as MM/dd/yy
+        moodGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(activity,
+            SimpleDateFormat("dd"))
+        moodGraph.gridLabelRenderer.numHorizontalLabels = 7
+        moodGraph.gridLabelRenderer.horizontalAxisTitle = "Date"
+        moodGraph.gridLabelRenderer.setHumanRounding(false)
+
+        // setting x axis divisions
+        moodGraph.viewport.isXAxisBoundsManual
+        moodGraph.viewport.setMinX(weekArray[0].time.toDouble())
+        moodGraph.viewport.setMaxX(weekArray[6].time.toDouble())
+
+        // TEXTVIEW DAILY AVERAGES
+
+        val dailyAverages = binding.analyticsDailyAverages
+        val sb = StringBuilder()
+
+        val dSleepText = "Daily Sleep (Hours): "
+        val dSleepValue = sleepDailyAverage
+
+        val dStepsText = "Daily Steps: "
+        val dStepsValue = stepsDailyAverage
+
+        val dMoodText = "Daily Mood: "
+        val dMoodValue = moodDailyAverage
+
+        dailyAverages.text = (sb.append(dSleepText).append(dSleepValue).append("\n")
+            .append(dStepsText).append(dStepsValue).append("\n")
+            .append(dMoodText).append(dMoodValue))
     }
 
     /**
